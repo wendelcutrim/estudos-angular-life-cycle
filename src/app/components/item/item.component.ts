0fb82caacd5d1ec436998a9d843fa098125d92b4
Wendel Cutrim <wendelcutrim@hotmail.com>
2023-05-28 13:00:19 -0300

    Estudos sobre o ngOnInit

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,18 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { Component, OnInit, Input } from "@angular/core";
+import { faPen, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { Item } from "src/app/interfaces/iItem";
 
 @Component({
-  selector: 'app-item',
-  templateUrl: './item.component.html',
-  styleUrls: ['./item.component.css']
+    selector: "app-item",
+    templateUrl: "./item.component.html",
+    styleUrls: ["./item.component.css"],
 })
 export class ItemComponent implements OnInit {
+    @Input() item!: Item;
 
-  faPen = faPen;
-  faTrash = faTrash
+    faPen = faPen;
+    faTrash = faTrash;
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void { }
-
+    ngOnInit(): void {}
 }
