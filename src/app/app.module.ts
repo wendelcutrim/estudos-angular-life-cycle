9ec886ad42cd56fa0d0b2a8baccab16241cc7863
Wendel Cutrim <wendelcutrim@hotmail.com>
2023-05-28 16:55:22 -0300

    Estudos sobre o hook ngOnChanges e como focar um elemento utilizando o decorator @ViewChild e a classe ElementRef do core do Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { InputComponent } from './components/input/input.component';
-import { ItemComponent } from './components/item/item.component';
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { InputComponent } from "./components/input/input.component";
+import { ItemComponent } from "./components/item/item.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    InputComponent,
-    ItemComponent
-  ],
-  imports: [
-    BrowserModule,
-    FontAwesomeModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, InputComponent, ItemComponent],
+    imports: [BrowserModule, FontAwesomeModule, FormsModule, ReactiveFormsModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,13 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { ListaDeCompraService } from "src/app/service/lista-de-compra.service";
 
 @Component({
-  selector: 'app-input',
-  templateUrl: './input.component.html',
-  styleUrls: ['./input.component.css']
+    selector: "app-input",
+    templateUrl: "./input.component.html",
+    styleUrls: ["./input.component.css"],
 })
 export class InputComponent implements OnInit {
+    item: string = "";
 
-  constructor() { }
+    constructor(private listaService: ListaDeCompraService) {}
+    @ViewChild("itemInput") input!: ElementRef; //Capturando a variável de template #itemInput para setar o focus.
 
-  ngOnInit(): void { }
+    ngOnInit(): void {}
+
+    adicionarItem() {
+        this.listaService.setListaDeCompra(this.item);
+        this.item = "";
+        this.input.nativeElement.focus(); //Setando o focus após enviar
+    }
 }
